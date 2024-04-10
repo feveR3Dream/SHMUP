0367b5e5a5af0a4107de050b3af9fcc91f44f4cd
feveR3Dream <147809541+feveR3Dream@users.noreply.github.com>
2024-04-10 12:20:33 +0700

    Update on SHMUP
    - Go home and finish later

--- Assets/Scripts/uiScripts/TextWriter.cs
+++ Assets/Scripts/uiScripts/TextWriter.cs
@@ -3,53 +3,78 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class TextWriter : MonoBehaviour
 {
-    private TextMeshProUGUI uiText;
-    private string textToWrite;
-    private int characterIndex;
-    private float timePerCharacter;
-    private float timer; // If a float value isn't assigned with anything, it's default value will be 0.0
-    private bool invisibleCharacters;
+    private List<TextWriterSingle> textWriterSingleList;
+
+    private void Awake()
+    {
+        textWriterSingleList = new List<TextWriterSingle>();
+    }
 
     public void AddWriter(TextMeshProUGUI uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
     {
-        this.uiText = uiText;
-        this.textToWrite = textToWrite;
-        this.timePerCharacter = timePerCharacter;
-        this.invisibleCharacters = invisibleCharacters;
-        characterIndex = 0;
+        textWriterSingleList.Add(new TextWriterSingle(uiText, textToWrite, timePerCharacter, invisibleCharacters));
     }
-    void Update()
+
+    private void Update()
     {
-        if (uiText != null)
+        for (int i = 0; i < textWriterSingleList.Count; i++)
         {
-            timer -= Time.deltaTime; // The value of timer being reduced every second.
-            while (timer <= 0)
+            textWriterSingleList[i].Update();
+        }
+    }
+
+    // Represents a single TextWriter instance
+    public class TextWriterSingle
+    {
+        private TextMeshProUGUI uiText;
+        private string textToWrite;
+        private int characterIndex;
+        private float timePerCharacter;
+        private float timer; // If a float value isn't assigned with anything, it's default value will be 0.0
+        private bool invisibleCharacters;
+
+        public TextWriterSingle(TextMeshProUGUI uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+        {
+            this.uiText = uiText;
+            this.textToWrite = textToWrite;
+            this.timePerCharacter = timePerCharacter;
+            this.invisibleCharacters = invisibleCharacters;
+            characterIndex = 0;
+        }
+        public void Update()
+        {
+            if (uiText != null)
             {
-                timer += timePerCharacter; // If timer hits 0 or lower, timer resets to the value of timePerCharacter.
-                characterIndex++; // Increase character index after each timer reset.
-                string text = textToWrite.Substring(0, characterIndex); // Print the previous character index before moving to a new one.
-
-                ///////////////////////|
-                if (invisibleCharacters) 
+                timer -= Time.deltaTime; // The value of timer being reduced every second.
+                while (timer <= 0)
                 {
-                    text += "<color=#00000000>" + textToWrite.Substring(characterIndex) + "</color>"; // [Need more time to understand and experiment]
-                }
-                ///////////////////////|
+                    timer += timePerCharacter; // If timer hits 0 or lower, timer resets to the value of timePerCharacter.
+                    characterIndex++; // Increase character index after each timer reset.
+                    string text = textToWrite.Substring(0, characterIndex); // Print the previous character index before moving to a new one.
 
-                uiText.text = text;
+                    ///////////////////////|
+                    if (invisibleCharacters)
+                    {
+                        text += "<color=#00000000>" + textToWrite.Substring(characterIndex) + "</color>"; // [Need more time to understand and experiment]
+                    }
+                    ///////////////////////|
 
-                if (characterIndex >= textToWrite.Length) // Avoid error message || Clamping the character index within the length of textToWrite.
-                {
-                    uiText = null;
-                    return;
+                    uiText.text = text;
+
+                    if (characterIndex >= textToWrite.Length) // Avoid error message || Clamping the character index within the length of textToWrite.
+                    {
+                        uiText = null;
+                        return;
+                    }
+
                 }
 
             }
 
         }
-        
     }
 }
